3ee25c63423a971977d2377378f06cf2a6eb2b7a
groone38 <groone38@yandex.ru>
2023-06-22 02:26:36 +0300

    lint in home from not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
 const NotFound = (props: Props) => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      NotFound page pleas go to{" "}
+      <Link to="/" style={{ color: "black" }}>
+        Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
